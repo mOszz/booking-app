0fad1efe4dfc0274c430291996e878b6e6485766
Victor.W <victor.wacquet07@gmail.com>
2023-09-07 14:16:37 +0200

    Update reservation class

--- src/main/java/com/dlog/bookingapp/models/Reservation.java
+++ src/main/java/com/dlog/bookingapp/models/Reservation.java
@@ -3,18 +3,30 @@ package com.dlog.bookingapp.models;
 import jakarta.persistence.*;
 import com.dlog.bookingapp.models.Customer;
 
+import java.util.Calendar;
+
 @Entity
 @Table(name="reservation")
 public class Reservation {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    // Clé étrangère vers Vehicule
     @ManyToOne
     @JoinColumn(name = "vehicule_id", referencedColumnName = "id")
+    @Column(nullable = false)
     private Vehicle vehicule;
     @ManyToOne
     @JoinColumn(name = "client_id", referencedColumnName = "id")
+    @Column(nullable = false)
     private Customer customer;
+    @Temporal(TemporalType.DATE)
+    @Column(nullable = false)
+    private Calendar startDate;
+    @Temporal(TemporalType.DATE)
+    @Column(nullable = false)
+    private Calendar endDate;
+    @Column(nullable = false)
+    private int kmCount;
     public void setId(Long id) {
         this.id = id;
     }
