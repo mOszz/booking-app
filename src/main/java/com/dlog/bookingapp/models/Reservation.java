efba05192f649b0b6972ec51fd152b8cebda23a4
Victor.W <victor.wacquet07@gmail.com>
2023-09-07 14:19:15 +0200

    Fix & complete reservation class

--- src/main/java/com/dlog/bookingapp/models/Reservation.java
+++ src/main/java/com/dlog/bookingapp/models/Reservation.java
@@ -34,4 +34,56 @@ public class Reservation {
     public Long getId() {
         return id;
     }
+
+    public Vehicle getVehicule() {
+        return vehicule;
+    }
+
+    public void setVehicule(Vehicle vehicule) {
+        this.vehicule = vehicule;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Calendar getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Calendar startDate) {
+        this.startDate = startDate;
+    }
+
+    public Calendar getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Calendar endDate) {
+        this.endDate = endDate;
+    }
+
+    public int getKmCount() {
+        return kmCount;
+    }
+
+    public void setKmCount(int kmCount) {
+        this.kmCount = kmCount;
+    }
+
+    @Override
+    public String toString() {
+        return "Reservation{" +
+                "id=" + id +
+                ", vehicule=" + vehicule +
+                ", customer=" + customer +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                ", kmCount=" + kmCount +
+                '}';
+    }
 }
