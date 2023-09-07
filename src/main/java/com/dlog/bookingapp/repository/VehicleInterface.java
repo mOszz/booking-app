75888503a1d7a9b9e5890f815976ccc34c72a9b8
Chaussinand <lebib07@live.fr>
2023-09-07 14:29:57 +0200

    interface and comments

--- src/main/java/com/dlog/bookingapp/repository/VehicleInterface.java
+++ src/main/java/com/dlog/bookingapp/repository/VehicleInterface.java
@@ -1,4 +1,11 @@
 package com.dlog.bookingapp.repository;
 
-public interface VehicleInterface {
+import com.dlog.bookingapp.models.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface VehicleInterface extends JpaRepository<Vehicle,Integer> {
+
 }
