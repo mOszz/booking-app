2b9ec9e6c62755ba7b37959cd28dc91976faa59e
Chaussinand <lebib07@live.fr>
2023-09-07 15:41:08 +0200

    create vehiculeService and vehiclesController

--- src/main/java/com/dlog/bookingapp/controllers/VehiclesController.java
+++ src/main/java/com/dlog/bookingapp/controllers/VehiclesController.java
@@ -1,4 +1,41 @@
 package com.dlog.bookingapp.controllers;
 
+import com.dlog.bookingapp.models.Vehicle;
+import com.dlog.bookingapp.services.VehicleService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/vehicles")
 public class VehiclesController {
+    private final VehicleService vehicleService;
+
+    public VehiclesController(VehicleService vehicleService) {
+        this.vehicleService = vehicleService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Vehicle>>getAllVehicules(){
+        return ResponseEntity.ok(vehicleService.getAllVehicules());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Vehicle>> fillById(@PathVariable int id){
+        return ResponseEntity.ok(vehicleService.vehicleById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Optional<Vehicle>> deleteVehicle(@PathVariable int id){
+        return ResponseEntity.ok(vehicleService.vehicleDelete(id));
+    }
+
+
+
+
+
+
+
 }
